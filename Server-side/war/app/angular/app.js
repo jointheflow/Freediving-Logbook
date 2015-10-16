56e69e807ae0f928a98cde096fa81ffaae050aa5
gianluca.ragnoni <gianluca.ragnoni@gmail.com>
2015-10-16 13:32:07 +0200

    Completed http invocation test

--- Server-side/war/app/angular/app.js
+++ Server-side/war/app/angular/app.js
@@ -1,8 +1,17 @@
 /*Creates all application module*/
-//freedivingLogbookModule is the main module that represents the app. It needs all other modules dependencies
-var freedivingLogbookModule = angular.module('freedivingLogbookModule', ['servicesModule', 'controllersModule']);
-//servicesModule contains all services
-var servicesModule = angular.module('servicesModule', []);
-//controllersModule contains all controllers
-var controllersModule = angular.module('controllersModule', []);
+//appNeaClient is the main module that represents the app. It needs all other modules dependencies
+var appNeaClient = angular.module('appNeaClient', ['appNeaClient.service']);
+var appNeaClientService = angular.module('appNeaClient.service', []);
 
+appNeaClient.run(function(freediverService) {
+    console.log('run after module loading');
+    console.log('invoking freediverService...');
+    var loginPromiseResponse = freediverService.login();
+    
+    loginPromiseResponse.success(function(data, status, headers, config) {
+    	console.log(data);
+    });
+    loginPromiseResponse.error(function(data, status, headers, config) {
+    	console.log(data);
+    });
+});
--- Server-side/war/app/angular/service/freediver_service.js
+++ Server-side/war/app/angular/service/freediver_service.js
@@ -1,15 +1,20 @@
 /*Contains wrapper of all freediver http services*/
 //get reference to the service module
-var servicesModule = angular.module('servicesModule');
+var appNeaClientService = angular.module('appNeaClient.service');
 
 /*add freediver services*/
-servicesModule.service('freediverService', function () {
+appNeaClientService.service('freediverService', function ($http) {
 	/*add login/signin service*/
 	this.login = function (externalPlatform, externalToken, platformId) {
-		return 'fake login data';
+		var loginResult;
+		return $http.get(freedivingLogbookConstant.apiHostName+freedivingLogbookConstant.apiFreediverLoginPath);
+		
 	};
+	
+	
+	
 	/*add remove service*/
 	this.remove = function (externalPlatform, externalToken, freediverId) {
-		
+		return 'fake remove action';
 	};
 });
