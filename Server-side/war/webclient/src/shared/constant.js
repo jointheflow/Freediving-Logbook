9f442c39723dcaa9241333ca53cf87be511df285
gianluca.ragnoni <gianluca.ragnoni@gmail.com>
2016-01-04 18:02:29 +0100

    managing facebook privacy policy

--- Server-side/war/webclient/src/shared/constant.js
+++ Server-side/war/webclient/src/shared/constant.js
@@ -1,5 +1,5 @@
 var freedivingLogbookConstant = freedivingLogbookConstant || {
-	applicationName: '><)))\xB0> app-nea',
+	applicationName: '><)))\xB0> appnea',
 	applicationClientVersion: '0.0.0 (grouperino)',
 	applicationServerVersion: '0.0.0 (grouperino)',
 
