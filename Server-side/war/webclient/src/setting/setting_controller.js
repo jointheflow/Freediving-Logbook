f838a7b7ab1044fb1d6ef3fd0f5b65123a485903
gianluca.ragnoni <gianluca.ragnoni@gmail.com>
2016-05-20 17:01:41 +0200

    Custom field management completed dialog for adding and remove field

--- Server-side/war/webclient/src/setting/setting_controller.js
+++ Server-side/war/webclient/src/setting/setting_controller.js
@@ -2,7 +2,7 @@
 var appNeaClient = angular.module('appNeaClient');
 
 /*controller definition */
-appNeaClient.controller('settingController',  function ($scope,$rootScope, modelService, $log) {
+appNeaClient.controller('settingController',  function ($scope,$rootScope, modelService, $log, $mdDialog) {
     
     $scope.freediver = modelService.freediverMdl;
     //$scope.customFieldListOfDiveSession = ["custom_one", "custom_two", "custom_three"];
@@ -12,6 +12,18 @@ appNeaClient.controller('settingController',  function ($scope,$rootScope, model
       { name: 'custom_three'}
     ];
     
+     /*check if the custom already exists in the list*/
+    $scope.customFieldExists = function() {
+        if ($scope.newCustom !=null && $scope.newCustom.length>0) {
+            for (var i = 0; i < $scope.customFieldListOfDiveSession.length; i++){
+                if ($scope.customFieldListOfDiveSession[i].name.toUpperCase() == $scope.newCustom.toUpperCase()){
+                    return true;
+                }
+            }
+            return false;
+        }else return true;
+        
+    }
     
     $scope.removeCustom = function() {
         if ($scope.custom != null) {
@@ -27,5 +39,56 @@ appNeaClient.controller('settingController',  function ($scope,$rootScope, model
             //$location.path('/divesessiondetail');
     };
     
-       
+   
+    $scope.dialogAdd = function() {
+        $log.info('add pressed for field name:'+$scope.newCustom);
+        //if ($scope.newCustom!=null && $scope.newCustom.length>0) {
+            $scope.customFieldListOfDiveSession.push({name: $scope.newCustom});
+        //}
+        $mdDialog.cancel();
+        
+    };
+    
+    $scope.dialogCancel = function() {
+        $log.info('cancel pressed');
+        $mdDialog.cancel();
+
+    };
+    
+   
+    $scope.addCustom = function() { 
+        
+        $mdDialog.show({
+                  templateUrl: 'src/setting/dialog_addcustomfield_view.html',
+                  parent: angular.element(document.body),
+                  clickOutsideToClose:true,
+                  fullscreen: false,
+                  preserveScope: true,
+                  scope: $scope    
+                  //locals: { dTitle: mTitle, dDetail: mDetail },
+                  /*controller: ['$scope', 'dTitle', 'dDetail', function($scope, dTitle, dDetail, $mdDialog) { 
+                        $scope.title = dTitle;
+                        $scope.detail = dDetail;
+                        $scope.close = function() {
+                            $mdDialog.cancel();
+                        };
+                  }]*/
+                  //controller : serviceDialogController
+                });
+
+            };
+
+           // serviceDialogController = function($scope, dTitle, dDetail, $mdDialog) {
+            serviceDialogController = function($scope, $mdDialog) {
+               /*$scope.title = dTitle;
+               $scope.detail = dDetail;  
+               */
+               $scope.close = function() {
+                            $mdDialog.cancel();
+               };
+                
+               
+            };
+    
+
 });
